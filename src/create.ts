557c5be1f53ddaf83d013888d3234ad4673d2080
许圣峰 <kricsleo@163.com>
2023-11-22 14:33:31 +0800

    feat: add spinner

--- src/create.ts
+++ src/create.ts
@@ -1,4 +1,5 @@
 import { defineCommand, runMain } from "citty";
+import { oraPromise } from 'ora'
 import { Template } from './types';
 import { version } from '../package.json'
 import { createTemplate } from './templates'
@@ -12,8 +13,20 @@ const main = defineCommand({
   },
   async run({ rawArgs }) {
     const [template, dist] = rawArgs
-    await createTemplate(template as Template, dist)
-    await install(dist)
+    const p1 = createTemplate(template as Template, dist)
+    oraPromise(p1, { 
+      text: 'Creating template',
+      successText: 'Template created',
+      failText: 'Failed to create template',
+    })
+    await p1
+    const p2 = install(dist)
+    oraPromise(p2, { 
+      text: 'Installing dependencies',
+      successText: 'Dependencies installed',
+      failText: 'Failed to install dependencies',
+    })
+    await p2
   },
 });
 
--- src/install.ts
+++ src/install.ts
@@ -1,7 +1,6 @@
 import { installDependencies, } from "nypm";
 
 export async function install(dist: string) {
-  // todo: add spinner
   await installDependencies({
     cwd: dist,
     silent: true
